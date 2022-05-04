95cbad425a8e77ad102d4cf68585a9e3339367d4
Florian <68273015+Florian-1801@users.noreply.github.com>
2022-05-04 16:00:04 +0200

    Update Items.lua
    Added Render Sprite function.

--- src/items/Items.lua
+++ src/items/Items.lua
@@ -385,3 +385,27 @@ function Items:Heritage(Mum, Dad)
     Graphics.Sprite("char_creator_portraits", Mum, CurrentMenu.X + 25 + (CurrentMenu.WidthOffset / 2), CurrentMenu.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, 228, 228)
     RageUI.ItemOffset = RageUI.ItemOffset + 228
 end
+
+---Render Sprite
+---@param TextureDictionary string
+---@param TextureName string
+---@param X number
+---@param Y number
+---@param Width number
+---@param Height number
+---@param Heading number
+---@param R number
+---@param G number
+---@param B number
+---@param A number
+---@return nil
+function Items:RenderSprite(TextureDictionary, TextureName, X, Y, Width, Height, Heading, R, G, B, A)
+    ---@type number
+    local X, Y, Width, Height = (tonumber(X) or 0) / 1920, (tonumber(Y) or 0) / 1080, (tonumber(Width) or 0) / 1920, (tonumber(Height) or 0) / 1080
+
+    if not HasStreamedTextureDictLoaded(TextureDictionary) then
+        RequestStreamedTextureDict(TextureDictionary, true)
+    end
+
+    DrawSprite(TextureDictionary, TextureName, X + Width * 0.5, Y + Height * 0.5, Width, Height, Heading or 255, tonumber(R) or 255, tonumber(G) or 255, tonumber(B) or 255, tonumber(A) or 200)
+end
